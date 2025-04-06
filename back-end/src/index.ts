77b1429257c89b88d004a92fe094739053aff346
Alice <sten123465@gmail.com>
2025-04-06 13:17:56 +0200

    added- import of data from test data

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,40 +2,96 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { usersTable } from './db/schema.ts';
+import { beatmap_sets, beatmaps } from './db/schema.ts';
 import { env } from 'process';
-import { sql } from 'drizzle-orm';
+import { eq, exists, sql } from 'drizzle-orm';
+import maps from '../Seed/maps.json' with {type: 'json'};
 
 const db = drizzle(`postgresql://postgres:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`)
-// console.log(await db.execute("select 1"))
 
 const app = new Hono()
-app.get('/', async (c) => {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John 51",
-    age: 30,
-    email: "Jogn@mail.org"
-  }
-  await db.insert(usersTable).values(user);
-  console.log("Welcome John 51");
-  const users = await db.select().from(usersTable);
-  console.log(users);
-  return c.text("Done!");
-})
 
 app.get('/test/random', async (c) => {
   return c.json({
-    user: await db.select().from(usersTable).orderBy(sql.raw('RANDOM()')).limit(2)
+    beatmaps: await db.select().from(beatmaps).orderBy(sql.raw('RANDOM()')).limit(2)
   });
 })
 
 app.get('/test/delete', async (c) => {
-  await db.delete(usersTable);
+  await db.delete(beatmaps);
+  await db.delete(beatmap_sets);
   return c.text("Table deleted");
 })
 
 app.get('/test/hello', (c) => {
   return c.text('Hello, World!!!');
+})
+
+app.get('/test/seed', async (c) => {
+  //This function really needs to be made broken out into sub functions
+  type mapType = typeof beatmaps.$inferInsert;
+  const mapsets: Map<number, number[]> = new Map<number, number[]>();
+  for (const m of maps){
+    const map: mapType = {
+      beatmap_id: Number(m.beatmap_id),
+      beatmapset_id: Number(m.beatmapset_id),
+      creator_id: Number(m.creator_id),
+      favourite_count: Number(m.favourite_count),
+      max_combo: Number(m.max_combo),
+      playcount: Number(m.playcount),
+      passcount: Number(m.passcount),
+      count_normal: Number(m.count_normal),
+      count_slider: Number(m.count_slider),
+      count_spinner: Number(m.count_spinner),
+      hit_length: Number(m.hit_length),
+
+      bpm: Number(m.bpm),
+      difficultyrating: Number(m.difficultyrating),
+      diff_aim: Number(m.diff_aim),
+      diff_speed: Number(m.diff_speed),
+      diff_size: Number(m.diff_size),
+      diff_overall: Number(m.diff_overall),
+      diff_approach: Number(m.diff_approach),
+      diff_drain: Number(m.diff_drain),
+      rating: Number(m.rating),
+      
+      approved: Number(m.approved),
+      mode: Number(m.mode),
+      genre_id: Number(m.genre_id),
+      language_id: Number(m.language_id),
+
+      storyboard: !!Number(m.storyboard),
+      video: !!Number(m.video),
+      download_unavailable: !!Number(m.download_unavailable),
+      audio_unavailable: !!Number(m.audio_unavailable),
+
+      title: m.title,
+      title_unicode: m.title_unicode,
+      artist: m.artist,
+      artist_unicode: m.artist_unicode,
+      version: m.version,
+      source: m.source,
+      creator: m.creator,
+      tags: m.tags,
+      file_md5: m.file_md5,
+
+      submit_date: m.submit_date,
+      approved_date: m.approved_date,
+      last_update: m.last_update,
+    };
+    await db.insert(beatmaps).values(map);
+    if(!mapsets.has(map.beatmapset_id)){
+      console.log(map.beatmapset_id);
+      mapsets.set(map.beatmapset_id, [map.beatmap_id]);
+      continue;
+    }
+    mapsets.get(map.beatmapset_id)?.push(map.beatmap_id);
+  }
+
+  mapsets.forEach(async (beatmaps, set_id) => {
+    await db.insert(beatmap_sets).values({beatmapset_id: set_id, beatmaps: beatmaps});
+  })
+  return c.text('Seed completed');
 })
 
 serve({
