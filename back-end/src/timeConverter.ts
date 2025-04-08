9c644fa4a226206f047ae3bce300a3e1547a9bf7
Alice <sten123465@gmail.com>
2025-04-08 14:32:57 +0200

    removed- console log

--- back-end/src/timeConverter.ts
+++ back-end/src/timeConverter.ts
@@ -2,6 +2,5 @@ export default function ParseMysqlDate(mysql: string | null): Date | null{
     if(mysql == null)
         return null;
     const returnValue = new Date(Date.parse(mysql + ' UTC+0'));
-    console.log(returnValue);
     return returnValue;
 }
