0c2e866165e7eb79f9b9906051de20d2092bd942
Alice <sten123465@gmail.com>
2025-04-15 13:53:28 +0200

    added- main element to page layout to space footer and header out from each other

--- front-end/app/routes/index.tsx
+++ front-end/app/routes/index.tsx
@@ -2,6 +2,7 @@
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { SiteHeader } from '../components/header/SiteHeader'
 import { SiteFooter } from '../components/footer/SiteFooter'
+import styles from './index.module.css'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -11,6 +12,7 @@ function Home() {
   const router = useRouter()
 
   return (<><SiteHeader/>
+  <main className={styles.mainPage}></main>
   <SiteFooter/></>
   )
 }
